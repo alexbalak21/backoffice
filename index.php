6d782bcbe5b39fec53ccc3aa9a406d7663a81635
Alex Balak <alex.balak@outlook.com>
2025-08-26 11:51:56 +0200

    integrating address

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Back-office</title>
+    
+</head>
+<body>
+    <div class="container">
+        <h1>Back-office</h1>
+        <a class="btn btn-primary" href="php_generator/generate_pdf.php">Generate PDF</a>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
